c03ed08fa279685ba8ac3ecd9e4bd9a736df62bf
YefersonDuque <duqueyeferson@gmail.com>
2025-05-28 20:19:31 -0500

    fin sesion 9

--- 07-heroes-spa/src/heroes/page/SearchPage.jsx
+++ 07-heroes-spa/src/heroes/page/SearchPage.jsx
@@ -2,20 +2,25 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useForm } from "../../../hooks/UseForm"
 import { HeroCards } from "../components"
 import queryString from "query-string";
+import { getHeroesByName } from "../helpers";
 
 export const SearchPage = () => {
 
   const navigate = useNavigate();
   const location = useLocation();
-  const {q = ''} = queryString.parse(location.search)  
+  const { q = '' } = queryString.parse(location.search)
+
+  const heroes = getHeroesByName(q);
+
+  const showSearch = (q.length === 0);
+  const showError = (q.length > 0) && (heroes.length === 0);
 
   const { searchText, onInputChange } = useForm({
-    searchText: ''
+    searchText: q
   });
 
   const onSearchSubmit = (event) => {
     event.preventDefault();
-    if (searchText.trim().length <= 1) return;
     navigate(`?q=${searchText}`)
   }
 
@@ -43,15 +48,25 @@ export const SearchPage = () => {
         <div className="col-7">
           <h4>Results</h4>
           <hr />
-          <div className="alert alert-primary">
-            Search a hero
-          </div>
 
-          <div className="alert alert-danger">
-            There's no results <b>{q}</b>
-          </div>
-
-          {/* <HeroCards {...hero}/>  */}
+          {/* {
+            (q === '' ? <div className="alert alert-primary">
+              Search a hero
+            </div> : (heroes.length === 0) && <div className="alert alert-danger">
+              There's no results <b>{q}</b>
+            </div>)
+          } */}
+          <div className="alert alert-primary animate__animated animate__fadeIn" style={{display: showSearch ? '' : 'none'}}>
+              Search a hero
+            </div>                            
+            <div className="alert alert-danger animate__animated animate__fadeIn " style={{display: showError ? '' : 'none'}}>
+              There's no results <b>{q}</b>
+            </div>
+          {
+            heroes.map(hero => (
+              <HeroCards key={hero.id} {...hero} />
+            ))
+          }
         </div>
       </div>
     </>
