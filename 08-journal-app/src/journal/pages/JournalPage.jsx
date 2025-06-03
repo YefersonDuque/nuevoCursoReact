4b0f06ccc7c0e1af615135f4e2f870fcb35cf986
YefersonDuque <duqueyeferson@gmail.com>
2025-06-03 10:40:18 -0500

    navbar - sidebar

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,12 +1,12 @@
 import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-    <Typography variant="h1">JournalPage</Typography>
-    <MailOutline/>
-    </>
+    <JournalLayout>
+      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam tenetur eveniet corporis quasi! Quis, facere asperiores odio eos quaerat ipsam, illo, quidem recusandae itaque aperiam minus accusantium debitis ex possimus!</Typography>
+    </JournalLayout>
   )
 }
 
--- 08-journal-app/src/theme/purpleTheme.js
+++ 08-journal-app/src/theme/purpleTheme.js
@@ -1,18 +1,20 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from '@mui/material';
 import { red } from '@mui/material/colors';
 
-// Create a theme instance.
 export const purpleTheme = createTheme({
-//   cssVariables: true,
-  palette: {
-    primary: {
-      main: '#262254',
-    },
-    secondary: {
-      main: '#543884',
-    },
-    error: {
-      main: red.A400,
-    },
-  },
-});
+    palette: {
+        primary: {
+            main: '#262254'
+        },
+        secondary: {
+            main: '#543884'
+        },
+        error: {
+            main: red.A400
+        }
+    }
+})
+
+
+
+
