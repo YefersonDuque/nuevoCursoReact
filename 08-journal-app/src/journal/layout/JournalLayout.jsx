75e098f534b028bc2fa88a980bf4896ab37ae4ff
YefersonDuque <duqueyeferson@gmail.com>
2025-06-03 11:52:51 -0500

    inicio de creación de botón flotante

--- 08-journal-app/src/journal/layout/JournalLayout.jsx
+++ 08-journal-app/src/journal/layout/JournalLayout.jsx
@@ -1,7 +1,6 @@
 import { Toolbar } from '@mui/material';
 import { Box } from '@mui/system'
-import { NavBar, SideBar } from '../components';
-
+import {NavBar, SideBar } from '../components'
 
 const drawerWidth = 280;
 
--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,11 +1,11 @@
-import { MailOutline } from "@mui/icons-material"
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NoteView, NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam tenetur eveniet corporis quasi! Quis, facere asperiores odio eos quaerat ipsam, illo, quidem recusandae itaque aperiam minus accusantium debitis ex possimus!</Typography>
+      <NothingSelectedView />
+      {/* <NoteView/> */}
     </JournalLayout>
   )
 }
